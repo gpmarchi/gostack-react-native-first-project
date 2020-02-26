6c75b800d577c7518d582f69f4dc36bc9e008cfe
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2020-02-26 17:19:43 -0300

    handle screen navigation using stack model

---
+++ src/routes.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Main from './pages/Main';
+import User from './pages/User';
+
+const Stack = createStackNavigator();
+
+const generalOptions = {
+  headerBackTitleVisible: false,
+  headerStyle: {
+    backgroundColor: '#7159c1',
+  },
+  headerTintColor: '#FFF',
+};
+
+function Routes() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Main" screenOptions={generalOptions}>
+        <Stack.Screen name="Main" component={Main} />
+        <Stack.Screen name="User" component={User} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default Routes;
